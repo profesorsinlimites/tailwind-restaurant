c60f65d773f5d1d2c428c663e1ee0bc260ae8505
profesorsinlimites <profesorsinlimites@gmail.com>
2020-02-29 14:14:23 -0400

    Pagina restaurant V.1

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'naranja-sinlimites': '#FF5733',
+        'grio-sinlimites': '#4C4C4C',
+        'grisc-sinlimites': '#7E7E7E',
+      },
+      fontFamily:{
+        'fsinlimites': ['Dancing Script','cursive'],
+        'breesinlimites': ['Bree Serif', 'serif'],
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
